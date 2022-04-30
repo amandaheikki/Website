8d9ea13247a059eb4156c459bac0fb76f68f1b7f
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2022-04-30 12:40:56 +0200

    changes

--- ogb_website/website/static/website/script.js
+++ ogb_website/website/static/website/script.js
@@ -1,7 +1,22 @@
-const toggleBtn = document.getElementsByClassName('toggle-button')[0]
+/* const toggleBtn = document.getElementsByClassName('toggle-bar')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleBtn.addEventListener('click', () =>
 {
     navbarLinks.classList.toggle('active')
-})
+}) 
+  */
+ 
+const header = document.querySelector("header");
+
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function(){
+    let currentScrollPos = window.scrollY;
+
+    prevScrollPos > currentScrollPos
+    ? header.classList.remove("scroll")
+    : header.classList.add("scroll")
+ 
+    prevScrollPos = currentScrollPos;
+};
