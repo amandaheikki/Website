7b19eaad2dfd1d0b36c1af4b2ded4d247e32a510
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2022-05-15 21:49:08 +0200

    Updates

--- ogb_website/website/static/website/script.js
+++ ogb_website/website/static/website/script.js
@@ -1,28 +1,3 @@
-/* const toggleBtn = document.getElementsByClassName('toggle-bar')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleBtn.addEventListener('click', () =>
-{
-    navbarLinks.classList.toggle('active')
-}) 
-  */
- 
-/* Creates a function that hides/displays the fullscreen menu when user is scrolling*/
-/* const navbar = document.querySelector("navbar");
-
-let prevScrollPos = window.scrollY;
-
-window.onscroll = function(){
-    let currentScrollPos = window.scrollY;
-
-    prevScrollPos > currentScrollPos
-    ? navbar.classList.remove("scroll")
-    : navbar.classList.add("scroll")
- 
-    prevScrollPos = currentScrollPos;
-}; */
-
-
 
 /* Creates a function that hides/displays the menu within a hamburger icon*/
 
