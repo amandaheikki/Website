3dcc7a401dbac1dbb36e3328081cd3a03c24fe63
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2022-06-22 20:22:16 +0200

    cleaned

--- ogb_website/website/static/website/script.js
+++ ogb_website/website/static/website/script.js
@@ -1,11 +1,2 @@
-
-/* Creates a function that hides/displays the menu within a hamburger icon*/
-function toggleClass(){
-  let menu = document.querySelector(".nav-list");
-  menu.classList.toggle("toggleCls");
-}
-
-let hamburger = document.querySelector(".ham-Icon");
-
-hamburger.addEventListener("click", toggleClass);
-
+function toggleClass(){let menu=document.querySelector(".nav-list");menu.classList.toggle("toggleCls")}
+let hamburger=document.querySelector(".ham-Icon");hamburger.addEventListener("click",toggleClass)
